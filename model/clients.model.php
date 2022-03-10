bdbd860db68938e96e3d6b3038860573e6a01336
Tomas Cogliatti <tomas.cogliatti@gmail.com>
2022-03-09 23:35:36 -0300

    avanzando con model de clientes get all paginado y altas

--- model/clients.model.php
+++ model/clients.model.php
@@ -1,4 +1,4 @@
-<php?
+<?php
 
 class ClientsModel {
     private $db;
@@ -7,7 +7,59 @@ class ClientsModel {
         $this->db = new PDO('mysql:host=localhost;'.'dbname=db_miu;charset=utf8','root', '');
     }
 
-    // ---- GET ALL
+    //////////////////////
 
-    function get()
+    function getAll($inicio, $items)
+    {
+        $query = $this->db->prepare(
+            'SELECT a.id, b.user, a.name, a.cuit_dni, a.datos_pago, a.contact, a.comment, a.date
+            FROM clientes AS a 
+            INNER JOIN users AS b 
+                ON a.user_fk = b.id
+            AND
+                ON a.cancel = null
+            ORDER BY a.name ASC 
+            LIMIT ' .$inicio. ', ' .$items  // paginado
+        );
+        $query->execute();
+        return $result = $query->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    function getOne($param)
+    {
+
+    }
+
+    function filter($param)
+    {
+
+    }
+
+    function add($param)
+    {
+            $query = $this->db->prepare(
+                'INSERT INTO clientes (user_fk, `name`, cuit_dni, datos_pago, mayo, contact, `comment`, `date`, cancel'
+                );
+            $query->execute([1,
+                            $param->name, 
+                            $param->cuit_dni, 
+                            $param->datos_pago,
+                            $param->mayo,
+                            $param->contact,
+                            $param->comment,
+                            null,
+                            0]);
+                            
+        return $this->db->lastInsertId();
+    }
+    function del($param)
+    {
+
+    }
+
+    function update($param)
+    {
+
+    }
+
 }
